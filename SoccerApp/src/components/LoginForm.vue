7f3f99a4c187de1b485ccf01bc66a6b88f4d46a3
Victor Martins <133995002+VictorMartins1310@users.noreply.github.com>
2024-10-25 22:40:03 +0200

    Form Optimized

--- SoccerApp/src/components/LoginForm.vue
+++ SoccerApp/src/components/LoginForm.vue
@@ -7,22 +7,24 @@ function login(){
     isAutenticated.value = !isAutenticated.value;
 }
 let loginData = {
-    email = "",
-    password = ""
+    email: "",
+    password: ""
 }
 
 </script>
 
 <template>
-<form v-if="isAutenticated === false" v-on:submit.prevent="login">
-    <input type="email" v-model=loginData.email placeholder="E-mail" />
-    <input type="password" v-model=loginData.password placeholder="Password" />
-    <button type="submit">Log me in</button>
-</form>
-<button v-if="isAutenticated === true" v-on:click="login">Logout</button>
+    <div class="login">
+        <form v-if="isAutenticated === false" v-on:submit.prevent="login">
+            <input type="email" v-model=loginData.email placeholder="E-mail" />
+            <input type="password" v-model=loginData.password placeholder="Password" />
+            <button type="submit">Log me in</button>
+        </form>
+        <button v-if="isAutenticated === true" v-on:click="login">Logout</button>
+    </div>
 </template>
 <style scoped>
-form {
+.form {
     padding: 5px;
 }
 </style>
