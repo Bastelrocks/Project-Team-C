0af1cafe8e8543adfc6765abf35d56b554c7deba
Victor Martins <133995002+VictorMartins1310@users.noreply.github.com>
2024-10-25 21:59:55 +0200

    V-model

--- SoccerApp/src/components/LoginForm.vue
+++ SoccerApp/src/components/LoginForm.vue
@@ -6,13 +6,17 @@ let isAutenticated = ref(false);
 function login(){
     isAutenticated.value = !isAutenticated.value;
 }
+let loginData = {
+    email = "",
+    password = ""
+}
 
 </script>
 
 <template>
 <form v-if="isAutenticated === false" v-on:submit.prevent="login">
-    <input type="email" placeholder="E-mail" />
-    <input type="password" placeholder="Password" />
+    <input type="email" v-model=loginData.email placeholder="E-mail" />
+    <input type="password" v-model=loginData.password placeholder="Password" />
     <button type="submit">Log me in</button>
 </form>
 <button v-if="isAutenticated === true" v-on:click="login">Logout</button>
