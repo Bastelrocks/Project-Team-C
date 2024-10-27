8b343767289491587f002b7d8d7e1af964b4af9e
VictorMartins <Victor.Martins.1310@hotmail.com>
2024-10-27 16:08:35 +0100

    Teams update

--- SoccerApp/src/views/TeamView.vue
+++ SoccerApp/src/views/TeamView.vue
@@ -1,7 +1,29 @@
 <script setup>
+import { ref } from 'vue';
+import { teamssStore } from '@/stores/teamsStore';
+import { countriesStore } from '@/stores/countriesStore';
+
+let teams = teamssStore();
+let countries = countriesStore();
+
+countries.getCountries();
+let countrieList = ref(countries.countrieList);
+let teamList = ref(teams.teamsList);
+
+let countrieID = ref(0);
+
+function getTeams(id){
+    teams.getTeams(id);
+    console.log(teamList.value);
+}
 
 </script>
 
 <template>
-    <h1>Under Construction</h1>
+    <h1>all Teams</h1>
+    <select v-model="countrieID" v-on:change="getTeams(countrieID)">
+        <option value=0>Select Countrie</option>
+        <option v-for="countrie in countrieList" :value=countrie.id>{{ countrie.id }} - {{ countrie.name }}</option>
+    </select>
+    <p v-for="team in teamList">{{ team.name }}</p>
 </template>
