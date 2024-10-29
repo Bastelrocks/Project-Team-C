d0bb7d5040699ce9be02dc96ce2e870ffc1fa679
Tim Rabisch <tim.rabisch@gmx.de>
2024-10-29 15:29:31 +0100

    displaying clubs in transferlist works with bugs

--- SoccerApp/src/views/TransferList.vue
+++ SoccerApp/src/views/TransferList.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { transferStore } from '@/stores/transferStore';
 import { teamssStore } from '@/stores/teamsStore';
 
@@ -8,26 +8,45 @@ let teams = teamssStore();
 
 transfer.getAllTransfers();
 let transferList = ref(transfer.transferList);
-const title = "Recent Transfers";
-defineEmits([title]);
+const title = ref("Recent Transfers");
 
-function getTeamName(id){
-    let teamName = "";
-    teamName = teams.getTeamName(id).then( t => {
-        console.log("T: " + t);
-        console.log("T.Name: " + t.name);
-        return t;
-    });
-    console.log(teamName);
-    return teamName;
-}
+let teamNames = ref({}); // Reactive map to store team names by ID
 
+const getTeamName = async (id) => {
+    try {
+        // Await the Promise to resolve and get the actual value
+        const t = await teams.getTeamName(id);
+        return t.name;
+    } catch (error) {
+        console.error('Error fetching team name:', error);
+        return null;
+    }
+};
 
+const populateTeamNames = async () => {
+    for (let transfer of transferList.value) {
+        if (!teamNames.value[transfer.clubOrigin]) {
+            teamNames.value[transfer.clubOrigin] = await getTeamName(transfer.clubOrigin);
+        }
+        if (!teamNames.value[transfer.clubDestination]) {
+            teamNames.value[transfer.clubDestination] = await getTeamName(transfer.clubDestination);
+        }
+    }
+};
+
+onMounted(async () => {
+    await populateTeamNames();
+});
 </script>
 
 <template>
-    <h1>Under Construction</h1>
+    <h1>{{ title }}</h1>
     <ul>
-        <li v-for="transfer in transferList">{{ transfer.playerID }} - {{ getTeamName(transfer.clubOrigin) }} - {{ getTeamName(transfer.clubDestination) }} - {{ transfer.value }}</li>
+        <li v-for="transfer in transferList" :key="transfer.uniqueID">
+            {{ transfer.playerID }} - 
+            {{ teamNames[transfer.clubOrigin] || 'Loading...' }} - 
+            {{ teamNames[transfer.clubDestination] || 'Loading...' }} - 
+            {{ transfer.value }}
+        </li>
     </ul>
 </template>
