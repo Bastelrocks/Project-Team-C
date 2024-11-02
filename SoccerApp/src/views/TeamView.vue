428641a67d96715c6cfb6e09e9c94e2585f9ce3b
Tim Rabisch <tim.rabisch@gmx.de>
2024-11-02 11:01:23 +0100

    updated teamview

--- SoccerApp/src/views/TeamView.vue
+++ SoccerApp/src/views/TeamView.vue
@@ -7,8 +7,8 @@ let teams = teamssStore();
 let countries = countriesStore();
 
 // Variables Definition
-let countrieID = ref(0);
-let countrieList = countries.countrieList;
+let countryID = ref(0);
+let countryList = countries.countrieList;
 let teamList = teams.teamsList;
 
 // Functions Section
@@ -25,12 +25,80 @@ teams.getAllTeams();
 </script>
 
 <template>
-    <h1>all Teams</h1>
-    <select v-model="countrieID" v-on:change="getTeams(countrieID)">
-        <option value=0 selected>Select Countrie</option>
-        <option v-for="countrie in countrieList" v-bind:value=countrie.id>{{ countrie.id }} - {{ countrie.name }}</option>
+    <div>
+    <div class="selectButton">
+        <select v-model="countryID" v-on:change="getTeams(countryID)">
+        <option value=0 selected>All Teams</option>
+        <option v-for="country in countryList" v-bind:value=country.id>Teams in {{ country.name }}</option>
     </select>
-    <ul>
-        <li v-for="team in teamList">{{ team.name }}</li>
-    </ul>
+    </div>
+    <table>
+            <thead class="tablehead">
+                <tr>
+                    <th>Logo</th>
+                    <th>Name</th>
+                    <th>Founded in</th>
+                    <th>Link to Players</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="team in teamList">
+                    <td v-if="team.image" style="text-align: center;" class="tableCell"><div class="playerImg"><img :src="item.image" class="playerImage"></div></td>
+                    <td v-else class="tableCell"><div class="playerImg"><img src="@/assets/football-player.svg" class="playerImage"></div></td>
+                    <td class="tableCell">{{ team.name }}</td>
+                    <td style="text-align: center;" class="tableCell">{{ team.foundationYear }}</td>
+                    <td class="tableCell"></td>
+                </tr>
+            </tbody>
+
+        </table>
+    </div>
 </template>
+
+<style scoped>
+table{
+    min-width: 500px;
+}
+h1 {
+    text-align: center;
+    color: rgb(208, 214, 24);
+}
+.tablePlayers{
+    display:flex;
+    justify-content: center;
+    color: hsla(160, 100%, 37%, 1);
+}
+.tablehead{
+    font-size: 1.4em;
+    text-decoration: underline;
+    font-weight: bold;
+}
+.playerImage{
+    width:30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.playerImg{
+    display: flex;
+    justify-content: center;
+}
+.tableCell{
+    border: 1px solid hsla(160, 100%, 37%, 1);;
+    padding: 0 5px;
+}
+select
+{
+  padding: 10px;
+  margin-bottom: 15px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  accent-color: brown;
+  width: 400px;
+ 
+} 
+.selectButton{
+    display: flex;
+    justify-content: center;
+}
+</style>
