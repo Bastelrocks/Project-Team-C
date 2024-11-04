e3345ed75575bfdf9044249da7f6c89c41425ba0
Victor Martins <Victor.Martins.1310@hotmail.com>
2024-11-04 16:29:57 +0100

    Transfer List Update

--- SoccerApp/src/stores/transferStore.js
+++ SoccerApp/src/stores/transferStore.js
@@ -10,10 +10,10 @@ export const transferStore = defineStore('transferStore', () => {
         let arrayLength = transferList.value.length;
         if (arrayLength > 0)
             for (let i = 0; i <= arrayLength; i++) {
-                let obj = transferList.value.pop();
+                transferList.value.pop();
             }
     }
-    
+
     async function getAllTransfers() {
         clearArray();
         const { data } = await supabase.from(tableName).select();
@@ -31,24 +31,24 @@ export const transferStore = defineStore('transferStore', () => {
             clubDestination: teams!clubDestination (name),
             id,
             transferDate,
-            value`);
+            marketValue`);
         if (error) {
             console.error(error);
         }
-        data.forEach(t => { transferList.value.push(t) });
-        
+        data.forEach(transferItem => { transferList.value.push(transferItem) });
     }
 
-    async function updatePlayer(playerID, clubID, value){
-        const { data } = await supabase.from(tableName).update(
-            {
-                clubID: clubID,
-                value: value
-            }
-        ).eq("playerID", playerID).select();
+    async function updatePlayer(playerID, clubID, value) {
+        let obj = {
+            clubID: clubID,
+            market_value: value
+        };
+        console.log("playerID: " + playerID + " clubID: ", clubID + " marketValue: " + value + "M€");
+        const { data } = await supabase.from("player").update(obj).eq("id", playerID).select();
+        console.table(data);
     }
 
-    async function getTransfer(id){
+    async function getTransfer(id) {
         const { data } = await supabase.from(tableName).select(`
             firstName: player!playerID (firstName),
             lastName: player!playerID (lastName),
@@ -56,19 +56,17 @@ export const transferStore = defineStore('transferStore', () => {
             clubDestination: teams!clubDestination (name),
             id,
             transferDate,
-            value`).eq("id", id);
+            marketValue`).eq("id", id);
         data.forEach(transfer => {
+            console.table(transfer);
             transferList.value.push(transfer);
         })
     }
-    async function addNewTransfer(transferObj){
+
+    async function addNewTransfer(transferObj) {
+        // console.table(transferObj);
         const { data, error } = await supabase.from(tableName).insert(transferObj).select();
-        data.forEach(transfer => {
-            getTransfer(transfer.id);
-        })
-       console.log(data.id);
-        await getTransfer(data.id);
-        updatePlayer(data.id, data.clubDestination, data.value);
+        updatePlayer(data[0].playerID, data[0].clubDestination, data[0].marketValue);
         transferList.value.push(data);
     }
     return { transferList, getAllTransfers, loadTransfers, addNewTransfer };
--- SoccerApp/src/views/TransferList.vue
+++ SoccerApp/src/views/TransferList.vue
@@ -1,47 +1,32 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { transferStore } from '@/stores/transferStore';
-import { teamsStore } from '@/stores/teamsStore';
 import { sessionStore } from '@/stores/sessionStore';
-import { playerStore } from '@/stores/playerStore';
+import AddNewTransfer from '@/components/AddNewTransfer.vue';
 
 let session = sessionStore();
 let transfer = transferStore();
-let teams = teamsStore();
-let players = playerStore();
 
-/* TODO:
- * Add new Transfer in TransferStore
- * In that don't forget to add the new object in the arrayList
- */
+const quantityPerSite = 15;
+let page = 1;
+let firstTransfer = ref(0), lastTransfer = ref(quantityPerSite - 1);
 
-//let today = new Date().toISOString().substring(0,10);
-let newTransfer = ref({
-    playerID: 0,
-    clubOrigin: 0,
-    clubDestination: 0,
-    transferDate: new Date().toISOString().substring(0,10),
-    value: 0
-})
+onMounted(() => {
+    transfer.loadTransfers();
+    if (transfer.transferList.length < quantityPerSite)
+        lastTransfer.value = transfer.transferList.length;
 
-teams.getAllTeams();
-transfer.loadTransfers();
+    });
 
-function getPlayersByClub(clubID){
-    players.getPlayersByClub(clubID);
-    teams.getTeamsWithout(clubID);
-}
-
-function onChangePlayer(playerID){
-    let newValue = 0;
-    newValue = players.getPlayer(playerID).then(
-         pl => {
-            newValue = pl.market_value;
-            newTransfer.value.value = pl.market_value;
-            return newValue;
-         }
-        )
+function viewTransfer(){
+    if (page !== 0){
+        firstTransfer.value = (page-1)*quantityPerSite;
+        lastTransfer.value = (page*quantityPerSite) - 1;
+        console.log("Page: " + page)
+        console.log("First Element: " + firstTransfer.value);
+        console.log("Last Element: " + lastTransfer.value);
     }
+}
 
 </script>
 
@@ -53,64 +38,52 @@ function onChangePlayer(playerID){
                 <th>Name</th>
                 <th>Club Destination</th>
                 <th>Transfer Date</th>
-                <th>Value</th>
+                <th>Transfer Value<br />In Millions €</th>
                 <th v-if="session.isAutenticated">Actions</th>
             </tr>
         </thead>
         <tbody>
-            <tr v-if="session.isAutenticated">
-                <td>
-                    <select v-model="newTransfer.clubOrigin" v-on:change="getPlayersByClub(newTransfer.clubOrigin)">
-                        <option value="0">Select Team</option>
-                        <option v-for="club in teams.teamsList" v-bind:value="club.idTeam">{{ club.idTeam }} - {{ club.name || "loading..." }}</option>
-                    </select>
-                </td>
-                <td>
-                    <select v-if="players.playerList.length === 0" v-model="newTransfer.playerID" disabled>
-                        <option value="0" disabled selected>No Players to Select</option>
-                    </select>
-                    <select v-else v-model="newTransfer.playerID" v-on:change="onChangePlayer(newTransfer.playerID)">
-                        <option value="0">Select Player</option>
-                        <option v-for="player in players.playerList" v-bind:value="player.id">{{ player.firstName + " " + player.lastName || "loading..." }}</option>
-                    </select>
-                </td>
-                <td>
-                    <select v-if="newTransfer.clubOrigin > 0" v-model="newTransfer.clubDestination">
-                        <option value="0">Select Team</option>
-                        <option v-for="club in teams.teamDestinationList" v-bind:value="club.idTeam">{{ club.name || "loading..." }}</option>
-                    </select>
-
-                    <select v-else v-model="newTransfer.clubDestination" disabled>
-                        <option value="0" selected>Select Team</option>
-                    </select>
-                </td>
-                <td>
-                    <input type="date" v-model="newTransfer.transferDate"/>
-                </td>
-                <td>
-                    <input class="transfervalue" type="number" v-model="newTransfer.value">
-                </td>
-                <td>
-                    <button v-on:click="transfer.addNewTransfer(newTransfer)">add Transfer</button>
-                </td>
+            <AddNewTransfer v-if="session.isAutenticated" />
+            <tr v-if="transfer.transferList.length === 0">
+                <td colspan="6">No Transfer available</td>
             </tr>
-            <tr v-for="transfer in transfer.transferList" v-bind:key="transfer.id">
+            <tr v-for="transfer in transfer.transferList.slice(firstTransfer.value, lastTransfer.value)" v-bind:key="transfer.id">
                 <td>{{ transfer.clubOrigin.name || "loading..." }}</td>
                 <td>{{ transfer.firstName.firstName + " " + transfer.lastName.lastName }}</td>
                 <td>{{ transfer.clubDestination.name || "loading..." }}</td>
                 <td>{{ transfer.transferDate }}</td>
-                <td>{{ transfer.value }}</td>
+                <td>{{ transfer.marketValue }}M €</td>
+                <td v-if="session.isAutenticated">
+                    <button>Edit 1</button>
+                    <button>Edit 2</button>
+                </td>
             </tr>
         </tbody>
+        <tfoot>
+            <tr>
+                <td v-if="page > 1" v-on:click="viewTransfer(page--)">back</td>
+                <td v-if="page > 1" colspan="3"></td>
+                <td v-if="page === 1" colspan="4"></td>
+                <td v-on:click="viewTransfer(page++)">next</td>
+            </tr>
+        </tfoot> 
     </table>
 </template>
 <style scoped>
+
 input{
     min-width: 150px;
 }
-.transfervalue {
-    min-width: 60px;
-    max-width: fit-content;
-    field-sizing: content; /*works only on chrome*/
+
+table{
+    border: 1px solid black;
+    border-radius: 4px;
+}
+
+td{
+    border: 1px solid black;
+    border-radius: 4px;
+    text-align: center;
+    padding: 3px;
 }
 </style>
