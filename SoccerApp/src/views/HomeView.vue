15585b20ee2e93ca42352415f174aef120df23ea
Tim Rabisch <tim.rabisch@gmx.de>
2024-11-07 21:17:58 +0100

    filled home with a little content

--- SoccerApp/src/views/HomeView.vue
+++ SoccerApp/src/views/HomeView.vue
@@ -1,19 +1,37 @@
 <script setup></script>
 <template>
   <div class="content">
-    <img alt="Soccer logo" class="logo" src="@/assets/soccerappLogo.svg" width="125" height="125" />
-    <h1>Welcome to the home of Soccerpage!</h1>
+    <img
+      alt="Soccer logo"
+      class="logo"
+      src="@/assets/soccerappLogo.svg"
+      width="125"
+      height="125"
+    />
+    <h1>Welcome to the home of Soccer!</h1>
+    <p>
+      This is the place where your soccer dreams come true! List players, create
+      transfers, add/delete players and teams. We are thrilled to have you here!
+      ⚽ Explore an extensive roster of soccer players from around the globe
+      right at your fingertips. Whether you're a fan, a scout, or just curious,
+      you're bound to find valuable insights. Join us to create the most stunning
+      data base the world of soccer has ever seen! We hope you have fun with our
+      page.
+    </p>
   </div>
 </template>
 
 <style scoped>
 .content {
-  margin-top: 15%;
+  margin-top: 10%;
 }
 
 .logo {
   display: block;
   margin: 0 auto 2rem;
 }
-
+p{font-size: 2em;}
+h1{
+  font-size: 3em;
+}
 </style>
